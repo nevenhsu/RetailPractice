d600c95dc416ac1859dfc5f71680487582706c83
Neven <space.man0318@gmail.com>
2019-07-01 22:19:30 +0800

    init

---
+++ retail/Classes/Store/CoreDataStore.swift
@@ -0,0 +1,101 @@
+//
+//  CoreDataStore.swift
+//  RetailStore
+//
+//  Created by Saravanan on 10/06/17.
+//  Copyright © 2017 Saravanan. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import RxSwift
+
+class CoreDataStore : NSObject {
+    static let sharedInstance = CoreDataStore()
+    var persistentStoreCoordinator : NSPersistentStoreCoordinator!
+    var managedObjectModel : NSManagedObjectModel!
+    var managedObjectContext : NSManagedObjectContext!
+    var cartItemsArray: Variable<[CartItem]> = Variable([])
+    
+    override init() {
+        managedObjectModel = NSManagedObjectModel.mergedModel(from: nil)
+        
+        persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        
+        let domains = FileManager.SearchPathDomainMask.userDomainMask
+        let directory = FileManager.SearchPathDirectory.documentDirectory
+        
+        let applicationDocumentsDirectory = FileManager.default.urls(for: directory, in: domains).first!
+        let options = [NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true]
+        
+        let storeURL = applicationDocumentsDirectory.appendingPathComponent("RetailStore.sqlite")
+        
+        try! persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
+        
+        managedObjectContext = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+        managedObjectContext.undoManager = nil
+        
+        super.init()
+    }
+    
+    func fetchCartItems(_ completionBlock: (([CartItem]) -> Void)!) {
+        self.fetchEntriesWithPredicate({ entries in
+            completionBlock(entries)
+        })
+    }
+    func fetchEntriesWithPredicate(_ completionBlock: (([CartItem]) -> Void)!) {
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult>  = NSFetchRequest(entityName: "CartItem")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "productId", ascending: true)]
+        fetchRequest.returnsObjectsAsFaults = false
+        managedObjectContext.perform {
+            let queryResults = try? self.managedObjectContext.fetch(fetchRequest)
+            let managedResults = queryResults! as! [CartItem]
+            self.cartItemsArray.value = managedResults
+            completionBlock(managedResults)
+        }
+    }
+    
+    //MARK: Database Operations
+    func save() {
+        do {
+            try managedObjectContext.save()
+        } catch let error {
+            print("error: \(error)")
+        }
+    }
+    
+    func deleteObject(cartItem: CartItem) {
+        managedObjectContext.delete(cartItem)
+    }
+    
+    func discardMOCChanges() {
+        managedObjectContext.rollback()
+    }
+    
+    //MARK: Utility Methods
+    func newCartItem() -> CartItem {
+        let entity = CartItem(context: managedObjectContext)
+        return entity
+        
+    }
+    
+    func checkForSimilarCartItemAndDelete(cartItemToCheck: CartItem) {
+        for cartItem in self.cartItemsArray.value {
+            if cartItem.productId == cartItemToCheck.productId && cartItemToCheck != cartItem {
+                deleteObject(cartItem: cartItem)
+            }
+        }
+    }
+    
+    func deleteCartItem(withProductId productId: Int16) {
+        for cartItem in self.cartItemsArray.value {
+            if cartItem.productId == productId {
+                deleteObject(cartItem: cartItem)
+                save()
+                break;
+            }
+        }
+    }
+    
+}
